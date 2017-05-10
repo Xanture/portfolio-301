31b01e137ce266c396ef727449d7da48b79917bd
Brian McFarlane <bmcfarlane82@gmail.com>
2017-05-09 19:37:52 -0700

    Hamberger menu working clear fix making magic happen, going home

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('hello world');
+
+var projectList = []
+
+function Project (projectTitle, projectlastUpdated, gitHubURL) {
+  this.projectTitle = projectTitle;
+  this.projectlastUpdated = projectlastUpdated;
+  this.gitHubURL = gitHubURL;
+  projectList.push(this);
+}
