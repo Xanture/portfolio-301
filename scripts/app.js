059b868ca9a8e7e487298234e76f544f2652990a
Brian McFarlane <bmcfarlane82@gmail.com>
2017-05-22 19:15:51 -0700

    created json logic to write to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,40 @@
 'use strict';
 
-var projectList = []
+var projects = [];
 
-function Project (projectTitle, projectlastUpdated, gitHubURL, projectDescription) {
-  this.projectTitle = projectTitle;
-  this.projectlastUpdated = projectlastUpdated;
-  this.gitHubURL = gitHubURL;
-  this.projectDescription = projectDescription;
-  projectList.push(this);
+function Project (rawDataObj) {
+  for (var key in rawDataObj) {
+    this[key] = rawDataObj[key];
+  }
 }
-$(document).ready(function(){
-  $('.fa-bars').on('click', function() {
-    $('.full-nav').slideToggle('slow', function() {
-      
-    })
+
+Project.prototype.toHtml = function() {
+  var template = $('#project-template').html();
+  var templateRender = Handlebars.compile(template);
+  return templateRender(this);
+};
+
+Project.loadAll = function(projectData) {
+  projectData.forEach(function(projectObject) {
+    projects.push(new Project(projectObject));
   });
-})
+
+  projects.forEach(function(project) {
+    $('#project-div').append(project.toHtml());
+  });
+}
+
+Project.fetchAll = function() {
+  if (localStorage.projectData) {
+    Project.loadAll(JSON.parse(localStorage.projectData));
+  } else {
+    $.getJSON('appData.json')
+    .then(function(data) {
+      Project.loadAll(data);
+      localStorage.projectData = JSON.stringify(data);
+    },
+    function(err) {
+      console.log(err);
+    })
+  }
+}
