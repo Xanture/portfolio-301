dd80d23a5579916ed79014c6b2a189b5333e11a1
Brian McFarlane <bmcfarlane82@gmail.com>
2017-05-28 16:57:55 -0700

    saved depdencies

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const requestProxy = require('express-request-proxy');
-
+const pg = require('pg');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+const conString = 'postgres://postgres:1234@localhost:5432/postgres';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
 app.use(express.static('./'));
 
 app.get('/index', function(request, response) {
