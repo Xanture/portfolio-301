69e3c545ebf65eb5da4e9ef23f4d30fd58882c49
Brian McFarlane <bmcfarlane82@gmail.com>
2017-05-28 17:06:52 -0700

    working on getting heroku to work

--- server.js
+++ server.js
@@ -1,22 +1,117 @@
 'use strict';
 
-const express = require('express');
-const requestProxy = require('express-request-proxy');
 const pg = require('pg');
-const app = express();
-
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const app = express();
 const conString = 'postgres://postgres:1234@localhost:5432/postgres';
+// const conString = 'postgres://postgres:freeze1019@localhost:5432/postgres'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
-app.use(express.static('./'));
 
-app.get('/index', function(request, response) {
-  console.log('is the server running?');
-  response.sendFile('/index.html', { root: '.'});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.get('/about', (request, response) => response.sendFile('new.html', {root: './public'}));
+
+app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
+app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
+app.get('/articles', (request, response) => {
+  client.query(`
+    SELECT * FROM articles
+    INNER JOIN authors
+      ON articles.author_id=authors.author_id;`
+  )
+  .then(result => response.send(result.rows))
+  .catch(console.error);
 });
 
-app.listen(PORT, function() {
-  console.log('is it?:' + PORT);
+app.post('/articles', function(request, response) {
+  client.query(
+    'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
+    [request.body.author, request.body.authorUrl],
+    function(err) {
+      if (err) console.error(err)
+      queryTwo()
+    }
+  )
+
+  function queryTwo() {
+    client.query(
+      `SELECT author_id FROM authors WHERE author=$1`,
+      [request.body.author],
+      function(err, result) {
+        if (err) console.error(err)
+        queryThree(result.rows[0].author_id)
+      }
+    )
+  }
+
+  function queryThree(author_id) {
+    client.query(
+      `INSERT INTO
+      articles(author_id, title, category, "publishedOn", body)
+      VALUES ($1, $2, $3, $4, $5);`,
+      [
+        author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body
+      ],
+      function(err) {
+        if (err) console.error(err);
+        response.send('insert complete');
+      }
+    );
+  }
 });
+
+app.put('/articles/:id', (request, response) => {
+  client.query(`
+    UPDATE authors
+    SET author=$1, "authorUrl"=$2
+    WHERE author_id=$3
+    `,
+    [request.body.author, request.body.authorUrl, request.body.author_id]
+  )
+  .then(() => {
+    client.query(`
+      UPDATE articles
+      SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
+      WHERE article_id=$6
+      `,
+      [
+        request.body.author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.body,
+        request.params.id
+      ]
+    )
+  })
+  .then(() => response.send('Update complete'))
+  .catch(console.error);
+});
+
+app.delete('/articles/:id', (request, response) => {
+  client.query(
+    `DELETE FROM articles WHERE article_id=$1;`,
+    [request.params.id]
+  )
+  .then(() => response.send('Delete complete'))
+  .catch(console.error);
+});
+
+app.delete('/articles', (request, response) => {
+  client.query('DELETE FROM articles')
+  .then(() => response.send('Delete complete'))
+  .catch(console.error);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
